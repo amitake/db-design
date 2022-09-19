9a541b941514d0e916ab222ac5c42c7362184c2f
amitake <s2022058@stu.musashino-u.ac.jp>
2022-09-19 22:41:00 +0900

    sql追加

--- flaskdb/dataaccess.py
+++ flaskdb/dataaccess.py
@@ -90,7 +90,7 @@ class DataAccess:
     # search seat data
     def search_seats(self):
         query = sql.SQL("""
-            SELECT * FROM \"seats\"
+            SELECT * FROM \"seats\" ORDER BY seat_name
         """)
         # self.show_sql(query)
         results = self.execute(query, autocommit=True)
@@ -124,3 +124,58 @@ class DataAccess:
             student.open_flg = r[4]
             student_list.append(student)
         return student_list
+
+    # search student data by student_num
+    def search_open_flg_by_studnet_num(self, student_num):
+        query = sql.SQL("""
+            SELECT open_flg FROM \"students\" WHERE student_num={student_num}
+        """).format(
+            student_num = sql.Literal(student_num)
+        )
+        # self.show_sql(query)
+        results = self.execute(query, autocommit=True)
+        open_flgs = []
+        for r in results:
+            student = Student()
+            student.open_flg = r[0]
+        print(student.open_flg)
+        return student.open_flg
+    
+        # search student data by student_num
+    def search_state_by_seat_name(self, seat_name):
+        query = sql.SQL("""
+            SELECT state FROM \"seats\" WHERE seat_name={seat_name}
+        """).format(
+            seat_name = sql.Literal(seat_name)
+        )
+        # self.show_sql(query)
+        results = self.execute(query, autocommit=True)
+        states = []
+        for r in results:
+            seat = Seat()
+            seat.state = r[0]
+        print(seat.state)
+        return seat.state
+    
+    # update seats data 
+    def update_open_flg(self, student_num, open_flg):
+        query = sql.SQL("""
+            UPDATE \"students\" SET open_flg={open_flg} WHERE student_num={student_num}
+        """).format(
+            open_flg = sql.Literal(open_flg),
+            student_num = sql.Literal(student_num)
+        )
+        # self.show_sql(query)
+        self.execute(query, autocommit=True)
+        
+        # update seats data 
+    def update_seats(self, student_num, seat_name, state):
+        query = sql.SQL("""
+            UPDATE \"seats\" SET student_num={student_num}, time=now(), state={state} WHERE seat_name={seat_name}
+        """).format(
+            student_num = sql.Literal(student_num),
+            state = sql.Literal(state),
+            seat_name = sql.Literal(seat_name)    
+        )
+        # self.show_sql(query)
+        self.execute(query, autocommit=True)
