2f5082360d7cc81aaa124a62e1e8758fce64b783
amitake <s2022058@stu.musashino-u.ac.jp>
2022-09-20 09:35:38 +0900

    sql更新

--- db/sql/students.sql
+++ db/sql/students.sql
@@ -7,3 +7,8 @@ CREATE TABLE students (
     open_flg INT,
     study_content VARCHAR(255)) without oids;
 INSERT INTO students (student_num, student_name, study_category, open_flg, study_content) VALUES (2022058, '竹中亜瞳', '画像処理', 1, '人物検出を用いたマーチングプレイヤーの位置情報による練習補助システムの構築');
+INSERT INTO students (student_num, student_name, study_category, open_flg, study_content) VALUES (2322001, '青井葦人', '自然言語処理', 1, 'サッカープレイヤーのプレイの言語化と技術向上曲線のなんちゃら');
+INSERT INTO students (student_num, student_name, study_category, open_flg, study_content) VALUES (2322002, '福岡太郎', 'VR', 1, 'なんちゃらこうちゃら');
+INSERT INTO students (student_num, student_name, study_category, open_flg, study_content) VALUES (2322003, '緑谷出久', '画像処理', 1, '個性認識システムの構築');
+INSERT INTO students (student_num, student_name, study_category, open_flg, study_content) VALUES (2322004, '爆豪勝己', '自然言語処理', 1, '爆発');
+INSERT INTO students (student_num, student_name, study_category, open_flg, study_content) VALUES (2322005, '金木研', 'データベース', 1, '白金木');
